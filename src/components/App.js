bc2441b94b76fbd5d6225682329f9cca32947aa1
ahmedali8 <imahmedalibhatti@gmail.com>
2021-01-07 12:20:45 +0500

    deployed to local network ganache

--- src/components/App.js
+++ src/components/App.js
@@ -1,85 +1,91 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 import './App.css';
-import Meme from '../abis/Meme.json'
+import Meme from '../abis/Meme.json';
 
-const ipfsClient = require('ipfs-http-client')
-const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
+const ipfsClient = require('ipfs-http-client');
+const ipfs = ipfsClient({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+}); // leaving out the arguments will default to these values
 
 class App extends Component {
-
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
   async loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      );
     }
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
+    const web3 = window.web3;
     // Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const networkId = await web3.eth.net.getId()
-    const networkData = Meme.networks[networkId]
-    if(networkData) {
-      const contract = web3.eth.Contract(Meme.abi, networkData.address)
-      this.setState({ contract })
-      const memeHash = await contract.methods.get().call()
-      this.setState({ memeHash })
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+    const networkId = await web3.eth.net.getId();
+    const networkData = Meme.networks[networkId];
+    if (networkData) {
+      const contract = web3.eth.Contract(Meme.abi, networkData.address);
+      this.setState({ contract });
+      const memeHash = await contract.methods.get().call();
+      this.setState({ memeHash });
     } else {
-      window.alert('Smart contract not deployed to detected network.')
+      window.alert('Smart contract not deployed to detected network.');
     }
   }
 
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       memeHash: '',
       contract: null,
       web3: null,
       buffer: null,
-      account: null
-    }
+      account: null,
+    };
   }
 
   captureFile = (event) => {
-    event.preventDefault()
-    const file = event.target.files[0]
-    const reader = new window.FileReader()
-    reader.readAsArrayBuffer(file)
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
     reader.onloadend = () => {
-      this.setState({ buffer: Buffer(reader.result) })
-      console.log('buffer', this.state.buffer)
-    }
-  }
+      this.setState({ buffer: Buffer(reader.result) });
+      console.log('buffer', this.state.buffer);
+    };
+  };
 
   onSubmit = (event) => {
-    event.preventDefault()
-    console.log("Submitting file to ipfs...")
+    event.preventDefault();
+    console.log('Submitting file to ipfs...');
     ipfs.add(this.state.buffer, (error, result) => {
-      console.log('Ipfs result', result)
-      if(error) {
-        console.error(error)
-        return
+      console.log('Ipfs result', result);
+      if (error) {
+        console.error(error);
+        return;
       }
-       this.state.contract.methods.set(result[0].hash).send({ from: this.state.account }).then((r) => {
-         return this.setState({ memeHash: result[0].hash })
-       })
-    })
-  }
+      this.state.contract.methods
+        .set(result[0].hash)
+        .send({ from: this.state.account })
+        .then((r) => {
+          return this.setState({ memeHash: result[0].hash });
+        });
+    });
+  };
 
   render() {
     return (
@@ -97,19 +103,26 @@ class App extends Component {
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
+              <div className="content mr-auto ml-auto mt-4">
+                <div
+                  style={{
+                    width: '200px',
+                    height: '200px',
+                    overflow: 'hidden',
+                    margin: '0 auto',
+                  }}
                 >
-                  <img src={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`} />
-                </a>
+                  <img
+                    style={{ width: '100%' }}
+                    src={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`}
+                    alt=""
+                  />
+                </div>
                 <p>&nbsp;</p>
                 <h2>Change Meme</h2>
-                <form onSubmit={this.onSubmit} >
-                  <input type='file' onChange={this.captureFile} />
-                  <input type='submit' />
+                <form onSubmit={this.onSubmit}>
+                  <input type="file" onChange={this.captureFile} />
+                  <input type="submit" />
                 </form>
               </div>
             </main>
